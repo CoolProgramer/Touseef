ab8646768352234e3bac799dec1d3767f21ea5e6
Touseef Ahmad <touseefahmad@A006-00718.local>
2023-02-20 19:11:11 +0500

    Updates

---
+++ _next/static/4fE-yrWCCr7Q6U_0wH3gW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-91a854d9c9cfa29b.js"],"/me":["static/chunks/979-8843d36cc03ca14f.js","static/chunks/pages/me-6c15fce41d08cd07.js"],sortedPages:["/_app","/_error","/me"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
