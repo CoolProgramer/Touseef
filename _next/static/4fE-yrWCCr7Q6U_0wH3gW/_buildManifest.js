0deb8f9b72b016b550077cc6f23da7ae685b8909
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2023-07-05 17:22:22 +0500

    Updates

--- _next/static/4fE-yrWCCr7Q6U_0wH3gW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-91a854d9c9cfa29b.js"],"/me":["static/chunks/979-8843d36cc03ca14f.js","static/chunks/pages/me-6c15fce41d08cd07.js"],sortedPages:["/_app","/_error","/me"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
